0497b27c526b00cd79e632363578cf492eb20769
texttechne <gedankengut@gmx.de>
2023-02-23 16:22:46 +0100

    feat: integrate Trippin service with simple table

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,15 +1,45 @@
 import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
 import formatter from "../model/formatter";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import FilterType from "sap/ui/model/FilterType";
 
 /**
  * @namespace org.odata2ts.tst.controller
  */
 export default class Main extends BaseController {
-	private formatter = formatter;
+  private formatter = formatter;
 
-	public sayHello() : void {
-		MessageBox.show("Hello World!");
-	}
+  private createSearchForm() {
+    return {
+      firstName: "",
+      lastName: "",
+      age: "",
+    };
+  }
 
+  onInit() {
+    this.onReset();
+  }
+
+  onReset() {
+    const searchForm = new JSONModel(this.createSearchForm(), false);
+    this.getView().setModel(searchForm, "searchForm");
+  }
+
+  onSearch() {
+    const searchForm = (this.getView().getModel("searchForm") as JSONModel).getData();
+
+    const filters = [
+      new Filter("FirstName", FilterOperator.Contains, searchForm.firstName),
+      new Filter("LastName", FilterOperator.Contains, searchForm.lastName),
+      // TODO: add other filters
+      // new Filter("ServiceLevel", FilterOperator.EQ, searchForm.dispositionType),
+      // new Filter("Prio", FilterOperator.EQ, searchForm.prio),
+    ];
+
+    this.getView().byId("peopleTable").getBinding("items").filter(filters, FilterType.Application);
+  }
 }
